3063c0237daa55e1f01f9962b9742320898e1c69
Hubert Grzesiak <hubert.grzesiak@outlook.com>
2018-10-27 14:10:14 +0200

    add

--- CodeAndCloud.Services/ContactService/ContactService.cs
+++ CodeAndCloud.Services/ContactService/ContactService.cs
@@ -8,6 +8,7 @@ using Microsoft.Azure; //Namespace for CloudConfigurationManager
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using CodeAndCloud.Services.ContactService;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace CodeAndCloud.Services.ContactServices
 {
@@ -27,27 +28,7 @@ namespace CodeAndCloud.Services.ContactServices
 
             db.Add<ContactModel>(contact);
             db.SaveChanges();
-
-            // Retrieve the storage account from the connection string.
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-                CloudConfigurationManager.GetSetting("DefaultEndpointsProtocol=https;AccountName=devcodeandcloud;AccountKey=9LhuuclWYiF5Oisb20idnhr8omIrghiGG8E4neyptO3MmXSRmjwVULkJslfzY4omSSdenU9ZS+0A8sv6XXsQtQ==;EndpointSuffix=core.windows.net"));
-
-            // Create the table client.
-            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
-
-            // Create the CloudTable object that represents the "people" table.
-            CloudTable table = tableClient.GetTableReference("people");
-
-            // Create a new customer entity.
-            CustomerEntity customer1 = new CustomerEntity(model.Name);
-            customer1.Email = model.Email;
-            customer1.PhoneNumber = model.PhoneNumber;
-
-            // Create the TableOperation object that inserts the customer entity.
-            TableOperation insertOperation = TableOperation.Insert(customer1);
-
-            // Execute the insert operation.
-            table.ExecuteAsync(insertOperation);
+           
         }
     }
 }
--- CodeAndCloud.Web/Controllers/HomeController.cs
+++ CodeAndCloud.Web/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure; // Namespace for CloudConfigurationManager
 using Microsoft.Azure.Storage; // Namespace for StorageAccounts
 using Microsoft.Azure.CosmosDB.Table; // Namespace for Table storage types
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace CodeAndCloud.Web.Controllers
 {
@@ -40,9 +42,27 @@ namespace CodeAndCloud.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Contact(AddContactViewModel model)
+        public async Task<IActionResult> ContactAsync(AddContactViewModel model)
         {
-            _service.Add(model);
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("DefaultEndpointsProtocol=https;AccountName=devcodeandcloud;AccountKey=9LhuuclWYiF5Oisb20idnhr8omIrghiGG8E4neyptO3MmXSRmjwVULkJslfzY4omSSdenU9ZS+0A8sv6XXsQtQ==;EndpointSuffix=core.windows.net"));
+
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.GetQueueReference("myqueue");
+
+            // Create the queue if it doesn't already exist.
+            await queue.CreateIfNotExistsAsync();
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage(model.Email);
+            await queue.AddMessageAsync(message);
+
+            _service.AddAsync(model);
             return View();
         }
 
